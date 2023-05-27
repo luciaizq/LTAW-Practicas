d7f611fb9165421e369ccf578a48a2147985ac5f
luciaizq <l.izquierdo.2019@alumnos.urjc.es>
2023-05-27 12:48:13 +0200

    practica 4 acabada

--- P4/chat/index.js
+++ P4/chat/index.js
@@ -1,38 +1,56 @@
 const electron = require('electron');
 
-const ip = require('ip');
-
 console.log("Hola desde el proceso de la web...");
 
 //-- Obtener elementos de la interfaz
-const btn_test = document.getElementById("btn_test");
 const display = document.getElementById("display");
 const info1 = document.getElementById("info1");
 const info2 = document.getElementById("info2");
 const info3 = document.getElementById("info3");
-const print = document.getElementById("print");
 
-//-- Acceder a la API de node para obtener la info
-//-- Sólo es posible si nos han dado permisos desde
-//-- el proceso princpal
+const Vnode = document.getElementById("Vnode");
+const Vchrome = document.getElementById("Vchrome");
+const Velectron = document.getElementById("Velectron");
+
+//Dirección IP y número de clientes
+const numeroclientes = document.getElementById("numeroclientes");
+const ip = document.getElementById("ip");
+
+//botón de prueba
+const btn_test = document.getElementById("btn_test");
+
+
 info1.textContent = process.arch;
 info2.textContent = process.platform;
 info3.textContent = process.cwd();
 
-console.log(ip.address());
-const ipAddressElement = document.getElementById('ip-address');
+
+Vnode.textContent = process.versions.node;
+Vchrome.textContent = process.versions.chrome;
+Velectron.textContent = process.versions.electron;
 
 btn_test.onclick = () => {
-    display.innerHTML += "TEST! ";
     console.log("Botón apretado!");
-
-    //-- Enviar mensaje al proceso principal
-    electron.ipcRenderer.invoke('test', "MENSAJE DE PRUEBA: Boton apretado");
+    electron.ipcRenderer.invoke('btn_test', "MENSAJE DE PRUEBA");
 }
 
 
-//-- Mensaje recibido del proceso MAIN
-electron.ipcRenderer.on('print', (event, message) => {
-    console.log("Recibido: " + message);
-    print.textContent = message;
-  });
+//Dirección IP
+electron.ipcRenderer.on("ip",(event,mensaje) => {
+  ip.innerHTML = mensaje + "/chat.html";
+  ip.href = mensaje + "/chat.html";
+});
+
+// Número de clientes
+numeroclientes.textContent = 0;
+electron.ipcRenderer.on("numeroclientes",(event, message) => {
+    numeroclientes.textContent = message;
+});
+
+electron.ipcRenderer.on("recibiendo",(event,message) => {
+  display.innerHTML += message + "</p>";
+});
+ 
+
+ 
+  
--- P4/chat/main.js
+++ P4/chat/main.js
@@ -1,63 +1,119 @@
-//-- Cargar el módulo de electron
 const electron = require('electron');
-const ip = require('ip');
 
 console.log("Arrancando electron...");
 
-//-- Variable para acceder a la ventana principal
-//-- Se pone aquí para que sea global al módulo principal
-let win = null;
-
-//-- Punto de entrada. En cuanto electron está listo,
-//-- ejecuta esta función
 electron.app.on('ready', () => {
     console.log("Evento Ready!");
 
-    //-- Crear la ventana principal de nuestra aplicación
+    // Ventana principal de nuestra aplicación
     win = new electron.BrowserWindow({
-        width: 600,   //-- Anchura 
-        height: 600,  //-- Altura
+        width: 1500,    
+        height: 1000,  
 
-        //-- Permitir que la ventana tenga ACCESO AL SISTEMA
+        // Acceso al sistema
         webPreferences: {
           nodeIntegration: true,
           contextIsolation: false
         }
     });
 
-  //-- En la parte superior se nos ha creado el menu
-  //-- por defecto
-  //-- Si lo queremos quitar, hay que añadir esta línea
-  //win.setMenuBarVisibility(false)
-
-  //-- Cargar contenido web en la ventana
-  //-- La ventana es en realidad.... ¡un navegador!
-  //win.loadURL('https://www.urjc.es/etsit');
-
-  //-- Cargar interfaz gráfica en HTML
+  // Cargar interfaz gráfica en HTML
   win.loadFile("index.html");
-  // Obtener y enviar la dirección IP del servidor
-  win.on('ready-to-show',() =>{
-    /*  const serverIp = require('ip').address(); */
-     win.webContents.send('serverIp', ip.address());
-     });
 
-  //-- Esperar a que la página se cargue y se muestre
-  //-- y luego enviar el mensaje al proceso de renderizado para que 
-  //-- lo saque por la interfaz gráfica
   win.on('ready-to-show', () => {
-    win.webContents.send('print', "MENSAJE ENVIADO DESDE PROCESO MAIN");
+    win.webContents.send('ip', 'http://' + ip.address() + ':' + PUERTO);
   });
 
-  //-- Enviar un mensaje al proceso de renderizado para que lo saque
-  //-- por la interfaz gráfica
-  win.webContents.send('print', "MENSAJE ENVIADO DESDE PROCESO MAIN");
+  // Enviar mensaje de prueba
+  electron.ipcMain.handle("btn_test", async(event, mensaje) => {
+    console.log(mensaje);
+    io.send("Hola a todos", mensaje);
+    win.webContents.send("recibiendo", "Prueba");
+  }
+  )
+});
+
+// Dependencias
+const socket = require('socket.io');
+const http = require('http');
+const express = require('express');
+const colors = require('colors');
+const ip = require('ip');
+
+// Definir el puerto
+const PUERTO = 9000;
+
+// Crear una nueva aplciacion web
+const app = express();
+
+// Servidor y servidor de websockets.
+const server = http.Server(app);
+const io = socket(server);
+
+app.get('/', (req, res) => {
+  res.send('Bienvenido' + '<p><a href="/chat.html">Chat</a></p>');
+});
+
+
+app.use('/', express.static(__dirname +'/'));
+
+// Ficheros estáticos
+app.use(express.static('public'));
+
+// Nueva conexión
+io.on('connect', (socket) => {
+  
+  console.log('Se ha conectado un usuario'.red);
+
+  win.webContents.send("numeroclientes",io.engine.clientsCount);
+
+  // Usuario dexconectado
+  socket.on('disconnect', function(){
+    console.log('Se ha desconectado un usuario'.red);
+
+    win.webContents.send("numeroclientes",io.engine.clientsCount);
+  
+  });  
+
+  socket.on("message", (msg)=> {
+    console.log(`Mensaje recibido!:` + msg.blue);
+    const command = msg.split("/")[1];
+
+switch (command) {
+  case 'help':
+    socket.send("Comandos soportados : /hello, /list, /hour y /date");
+    break;
+
+  case 'hello':
+    socket.send('Bienvenido');
+  break;
+
+  case 'list':
+    const users = 'Se ha desconectado un usuario: ' + io.engine.clientsCount;
+    socket.send(users);
+    break;
+
+  case 'hour':
+    const currentTime = new Date();
+    const timeString = 'Hora: ' + currentTime.toLocaleTimeString();
+    socket.send(timeString);
+  break;
+
+  case 'date':
+    const currentDate = new Date();
+    const dateString = 'Fecha: ' + currentDate.toLocaleDateString();
+    socket.send(dateString);
+    break;
+
+  default:
+    io.send(msg);
+    break;
+    win.webContents.send("recibiendo", msg);
+}
+  });
 
 });
 
 
-//-- Esperar a recibir los mensajes de botón apretado (Test) del proceso de 
-//-- renderizado. Al recibirlos se escribe una cadena en la consola
-electron.ipcMain.handle('test', (event, msg) => {
-  console.log("-> Mensaje: " + msg);
-});
+server.listen(PUERTO);
+console.log("Escuchando en puerto: " + PUERTO);
